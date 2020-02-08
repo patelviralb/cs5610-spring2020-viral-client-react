0953fe760494d4de0c877f615456754892589d81
viralbpatel <patel.viral@husky.neu.edu>
2020-02-08 15:31:01 -0500

    Row seletion criteria updated

--- src/components/CourseManager/ListView/CourseTableRowComponent.js
+++ src/components/CourseManager/ListView/CourseTableRowComponent.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../../../styles/course-table-style-client.css"
-import {getDate} from "../../../common/constants";
+import { getDate } from "../../../common/constants";
 
 class CourseTableRowComponent extends React.Component {
   state = {
@@ -44,84 +44,64 @@ class CourseTableRowComponent extends React.Component {
     this.props.updateCourse(this.state.course)
   };
 
-  selectRow = () => {
-    this.setState((previousState) => {
-      if (this.state.isRowSelected) {
-        return {
-          isRowSelected: false
-        }
-      } else {
-        return {
-          isRowSelected: true
-        }
-      }
-    });
-  };
-
   render() {
     return (
-        <tr className={`${this.state.isRowSelected || this.state.isEdit
-            ? "vp-cs5610-highlight-row"
-            : ""}`} onClick={this.selectRow}>
-          <td className="vp-cs5610-title-column-width pl-5 pt-4">
+      <tr className={`${this.state.isEdit ? "vp-cs5610-highlight-row" : ""}`}>
+        <td className="vp-cs5610-title-column-width pl-5 pt-4">
+          {
+            !this.state.isEdit &&
+            <div className="d-flex justify-content-start w-75">
+              <i className="fas fa-book mt-1"></i>
+              <button
+                className="btn btn-link mt-n2"
+                title={this.state.course.courseTitle}
+                onClick={this.props.showCourseEditorPage}
+              >
+                {this.state.course.courseTitle}
+              </button>
+            </div>
+          }
+          {
+            this.state.isEdit &&
+            <input type="text" id="vp-cs5610-course-name-to-add"
+              className="form-control mr-sm-2 w-100 mt-n2"
+              placeholder="New Course Title"
+              maxLength="30"
+              onChange={this.updateCourseTitle}
+              value={this.state.course.courseTitle} />
+          }
+        </td>
+        <td className="d-sm-table-cell d-none vp-cs5610-owner-column-width pt-4">Me</td>
+        <td className="d-md-table-cell d-none pt-4 vp-cs5610-modified-column-width">
+          {this.state.course.dateModified}
+        </td>
+        <td className="vp-cs5610-extra-column-width">
+          <div className="float-right float-md-none">
+            <button className="ml-2 mt-2 btn btn-danger"
+              title="Delete Course"
+              onClick={() => this.props.deleteCourse(
+                this.state.course._id)}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
             {
               !this.state.isEdit &&
-              <div className="d-flex justify-content-start w-75">
-                <i className="fas fa-book mt-1"></i>
-                <button
-                    className="btn btn-link mt-n2"
-                    title={this.state.course.courseTitle}
-                    onClick={this.props.showCourseEditorPage}
-                >
-                  {this.state.course.courseTitle}
-                </button>
-              </div>
+              <button className="ml-2 mt-2 btn btn-warning"
+                title="Edit Course Title"
+                onClick={this.editCourseTitle}>
+                <i className="fas fa-edit"></i>
+              </button>
             }
             {
               this.state.isEdit &&
-              <input type="text" id="vp-cs5610-course-name-to-add"
-                     className="form-control mr-sm-2 w-100 mt-n2"
-                     placeholder="New Course Title"
-                     maxLength="30"
-                     onChange={this.updateCourseTitle}
-                     value={this.state.course.courseTitle}/>
+              <button className="ml-2 mt-2 btn btn-success"
+                title="Update Course Title"
+                onClick={this.updateCourse}>
+                <i className="fas fa-check"></i>
+              </button>
             }
-          </td>
-          <td className="d-sm-table-cell d-none vp-cs5610-owner-column-width pt-4">Me</td>
-          <td className="d-md-table-cell d-none pt-4 vp-cs5610-modified-column-width">
-            {this.state.course.dateModified}
-          </td>
-          <td className="vp-cs5610-extra-column-width">
-            {
-              (this.state.isRowSelected || this.state.isEdit)
-              &&
-              <div className="float-right float-md-none">
-                <button className="ml-2 mt-2 btn btn-danger"
-                        title="Delete Course"
-                        onClick={() => this.props.deleteCourse(
-                            this.state.course._id)}>
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-                {
-                  !this.state.isEdit &&
-                  <button className="ml-2 mt-2 btn btn-warning"
-                          title="Edit Course Title"
-                          onClick={this.editCourseTitle}>
-                    <i className="fas fa-edit"></i>
-                  </button>
-                }
-                {
-                  this.state.isEdit &&
-                  <button className="ml-2 mt-2 btn btn-success"
-                          title="Update Course Title"
-                          onClick={this.updateCourse}>
-                    <i className="fas fa-check"></i>
-                  </button>
-                }
-              </div>
-            }
-          </td>
-        </tr>
+          </div>
+        </td>
+      </tr>
     )
   }
 }
