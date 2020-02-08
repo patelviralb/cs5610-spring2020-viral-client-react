cafb3d845f7a06c1542749e685df1b2af4c250b7
viralbpatel <patel.viral@husky.neu.edu>
2020-02-08 17:32:16 -0500

    Course name display complete

--- src/components/CourseEditor/CourseEditorComponent.js
+++ src/components/CourseEditor/CourseEditorComponent.js
@@ -3,15 +3,18 @@ import CourseEditorNavigationBar from "./CourseEditorNavigationBarComponent";
 import CourseModuleComponent from "./CourseModules/CourseModuleComponent";
 import CourseModuleDetails from "./CourseModules/ModuleDetails/ModuleDetailsComponent";
 
-const CourseEditorComponent = ({ history }) =>
-    <div>
-      <CourseEditorNavigationBar history={history} />
-      <div className="container-fluid ml-n3 mr-n3 h-100">
-        <div className="row vp-cs5610-course-editor-division h-100">
-          <CourseModuleComponent />
-          <CourseModuleDetails />
-        </div>
+const CourseEditorComponent = ({ history, match }) =>
+  <div>
+    <CourseEditorNavigationBar
+      history={history}
+      match={match}
+    />
+    <div className="container-fluid ml-n3 mr-n3 h-100">
+      <div className="row vp-cs5610-course-editor-division h-100">
+        <CourseModuleComponent />
+        <CourseModuleDetails />
       </div>
     </div>
+  </div>
 
 export default CourseEditorComponent
--- src/components/CourseEditor/CourseEditorNavigationBarComponent.js
+++ src/components/CourseEditor/CourseEditorNavigationBarComponent.js
@@ -1,20 +1,40 @@
 import React from "react"
-import "../../styles/course-editor-style-client.css"  
+import "../../styles/course-editor-style-client.css"
+import { findCourseById } from "../../service/CourseService"
 
-const CourseEditorNavigationBar = ({ history }) =>
-  <nav className="navbar navbar-dark bg-dark">
-    <div
-      className="d-flex flex-row my-0 justify-content-start pt-sm-3 pt-md-0">
-      <button
-        className="btn btn-sm btn-dark close vp-cs5610-close-course-editor"
-        onClick={() => history.push("/")}
-      >
-        <span className="p-3 text-white">&#10005;</span>
-      </button>
-      <span
-        className="p-2 vp-cs5610-course-editor-span ml-2">CS5610 - Web Development
-        </span>
-    </div>
-  </nav>
+class CourseEditorNavigationBar extends React.Component {
+  state = {
+    course: []
+  }
+
+  componentDidMount = async () => {
+    const selectedCourseID = this.props.match.params.courseID
+    const selectedCourse = await findCourseById(selectedCourseID);
+    this.setState({
+      course: selectedCourse
+    });
+  };
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <div
+          className="d-flex flex-row my-0 justify-content-start pt-sm-3 pt-md-0">
+          <button
+            className="btn btn-sm btn-dark close vp-cs5610-close-course-editor"
+            onClick={() => this.props.history.push("/")}
+          >
+            <span className="p-3 text-white">&#10005;</span>
+          </button>
+          <span
+            className="p-2 vp-cs5610-course-editor-span ml-2"
+          >
+            {this.state.course.courseTitle}
+          </span>
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default CourseEditorNavigationBar
