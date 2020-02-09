1ffad46710b63493f1712661909d328b4fca810c
viralbpatel <patel.viral@husky.neu.edu>
2020-02-09 18:28:02 -0500

    TopicReducer added

---
+++ src/reducers/TopicReducer.js
@@ -0,0 +1,52 @@
+import TopicActions from "../actions/TopicActions"
+
+const initialState = {
+    topics: []
+}
+
+const TopicReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case TopicActions.FIND_TOPICS_FOR_MODULE:
+            return {
+                topics: action.allFoundTopics
+            }
+        case TopicActions.CREATE_NEW_TOPIC:
+            return {
+                topics: [
+                    ...state.topics,
+                    action.newAddedTopic
+                ],
+                selectedTopicID: state.selectedTopicID
+            }
+        case TopicActions.DELETE_TOPIC:
+            return {
+                topics: state.topics.filter(topic => topic._id !== action.topicID),
+                selectedTopicID: state.selectedTopicID
+            }
+        case TopicActions.UPDATE_TOPIC:
+            const index = state.topics.findIndex((topic) => topic._id === action.topicID)
+            return {
+                topics: [
+                    ...state.topics.slice(0, index),
+                    action.updatedTopic,
+                    ...state.topics.slice(index + 1)
+                ],
+                selectedTopicID: state.selectedTopicID
+            }
+        case TopicActions.SELECT_TOPIC:
+            return {
+                topics: [
+                    ...state.topics
+                ],
+                selectedTopicID: action.selectedTopicID
+            }
+        case TopicActions.REMOVE_ALL_TOPICS:
+            return {
+                topics: []
+            }
+        default:
+            return state
+    }
+}
+
+export default TopicReducer
