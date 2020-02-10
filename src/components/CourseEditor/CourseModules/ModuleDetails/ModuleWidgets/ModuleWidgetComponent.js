7e51c546da885022d03f11c8cd8b9199ec7dfcdd
viralbpatel <patel.viral@husky.neu.edu>
2020-02-09 20:51:55 -0500

    Widget Hide based on Topic Selection

--- src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/ModuleWidgetComponent.js
+++ src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/ModuleWidgetComponent.js
@@ -1,23 +1,47 @@
-import React from "react"
-import EachWidget from "./EachWidgets/EachWidgetComponent"
+import React from "react";
+import EachWidget from "./EachWidgets/EachWidgetComponent";
+import { connect } from "react-redux";
 
-const ModuleWidget = () =>
-    <div className="container-fluid">
-      <EachWidget/>
-
-      <div className="row">
-        <div className="col-12 d-flex justify-content-end pt-4">
-          <button className="btn btn-warning ml-2">
-            <i className="fas fa-plus"></i>
-          </button>
-        </div>
+class ModuleWidget extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.selectedTopicID
+          &&
+          <div className="container-fluid" >
+            <EachWidget />
+            {
+              this.props.selectedTopicID
+              &&
+              <div className="row">
+                <div className="col-12 d-flex justify-content-end pt-4">
+                  <button className="btn btn-warning ml-2">
+                    <i className="fas fa-plus"></i>
+                  </button>
+                </div>
+              </div>
+            }
+            {
+              this.props.selectedTopicID
+              &&
+              <div className="row">
+                <div className="col-12 pb-4">
+                  {/*For Padding*/}
+                </div>
+              </div>
+            }
+          </div>
+        }
       </div>
+    )
+  }
+}
 
-      <div className="row">
-        <div className="col-12 pb-4">
-          {/*For Padding*/}
-        </div>
-      </div>
-    </div>
+const stateToPropertyMapper = (state) => {
+  return {
+    selectedTopicID: state.topicReducer.selectedTopicID
+  }
+}
 
-export default ModuleWidget
+export default connect(stateToPropertyMapper)(ModuleWidget)
--- src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/WidgetPreviewSaveComponent.js
+++ src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/WidgetPreviewSaveComponent.js
@@ -1,19 +1,37 @@
-import React from "react"
+import React from "react";
+import { connect } from "react-redux";
 
-const WidgetPreviewSave = () =>
-    <div className="row">
-      <div className="col-12 d-flex justify-content-end">
-        <div className="custom-control custom-switch pt-2">
-          <input type="checkbox" className="custom-control-input"
-                 id="vp-cs5610-preview-switch" />
-            <label className="custom-control-label"
-                   htmlFor="vp-cs5610-preview-switch">Preview</label>
-        </div>
-        <label
-            className="form-control btn btn-success ml-4 w-auto d-flex justify-content-center vp-cs5610-save-topic">
-          Save
-        </label>
+class WidgetPreviewSave extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.selectedTopicID
+          &&
+          <div className="row">
+            <div className="col-12 d-flex justify-content-end">
+              <div className="custom-control custom-switch pt-2">
+                <input type="checkbox" className="custom-control-input"
+                  id="vp-cs5610-preview-switch" />
+                <label className="custom-control-label"
+                  htmlFor="vp-cs5610-preview-switch">Preview</label>
+              </div>
+              <label
+                className="form-control btn btn-success ml-4 w-auto d-flex justify-content-center vp-cs5610-save-topic">
+                Save
+            </label>
+            </div>
+          </div>
+        }
       </div>
-    </div>
+    )
+  }
+}
 
-export default WidgetPreviewSave
+const stateToPropertyMapper = (state) => {
+  return {
+    selectedTopicID: state.topicReducer.selectedTopicID
+  }
+}
+
+export default connect(stateToPropertyMapper)(WidgetPreviewSave)
