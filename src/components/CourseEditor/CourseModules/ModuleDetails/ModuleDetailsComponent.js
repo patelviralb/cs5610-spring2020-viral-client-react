c4f39b3511ec331e132d1ecf6c4b5907350a157c
viralbpatel <patel.viral@husky.neu.edu>
2020-02-11 12:21:18 -0500

    ModuleLesson component rename

--- src/components/CourseEditor/CourseModules/ModuleDetails/ModuleDetailsComponent.js
+++ src/components/CourseEditor/CourseModules/ModuleDetails/ModuleDetailsComponent.js
@@ -1,12 +1,12 @@
 import React from "react"
-import CourseModuleLessons from "./ModuleTabs/ModuleLessonsComponent";
+import ModuleLessons from "./ModuleTabs/ModuleLessonsComponent";
 import TabPills from "./ModulePills/LessonTopicsComponent";
 import WidgetPreviewSave from "./ModuleWidgets/WidgetPreviewSaveComponent"
 import ModuleWidget from "./ModuleWidgets/ModuleWidgetComponent"
 
 const CourseModuleDetails = () =>
     <div className="col-lg-9 col-12 ml-2 ml-lg-0">
-      <CourseModuleLessons/>
+      <ModuleLessons/>
       <div>
         <TabPills/>
       </div>
