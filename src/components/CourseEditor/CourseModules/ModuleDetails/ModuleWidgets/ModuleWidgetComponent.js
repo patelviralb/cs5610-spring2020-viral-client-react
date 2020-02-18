024024ffed1ef29e7ee50bdea447d8d8b74d281e
viralbpatel <patel.viral@husky.neu.edu>
2020-02-17 20:04:01 -0500

    Create new widget functionality added

--- src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/ModuleWidgetComponent.js
+++ src/components/CourseEditor/CourseModules/ModuleDetails/ModuleWidgets/ModuleWidgetComponent.js
@@ -1,47 +1,93 @@
 import React from "react";
 import EachWidget from "./EachWidgets/EachWidgetComponent";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
+import {createNewWidget} from "../../../../../actions/WidgetActions";
+import WidgetService from "../../../../../service/WidgetService";
 
 class ModuleWidget extends React.Component {
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(prevProps.widgetID);
+  }
+
   render() {
     return (
-      <div>
-        {
-          this.props.selectedTopicID
-          &&
-          <div className="container-fluid" >
-            <EachWidget />
-            {
-              this.props.selectedTopicID
-              &&
-              <div className="row">
-                <div className="col-12 d-flex justify-content-end pt-4">
-                  <button className="btn btn-warning ml-2">
-                    <i className="fas fa-plus"></i>
-                  </button>
+        <div>
+          {
+            (this.props.selectedTopicID !== null && this.props.widgetList)
+            &&
+            <div className="container-fluid">
+              {
+                this.props.widgetList.map((widgetList) => {
+                  return <EachWidget
+                      widgetList={widgetList}
+                      key={widgetList.id}
+                      history={this.props.history}
+                      match={this.props.match}
+                  />
+                })
+              }
+              {
+                this.props.selectedTopicID
+                &&
+                <div className="row">
+                  <div className="col-12 d-flex justify-content-end pt-4">
+                    <button
+                        className="btn btn-warning ml-2"
+                        onClick={() => this.props.createNewWidget(
+                            this.props.selectedTopicID)}
+                    >
+                      <i className="fas fa-plus"></i>
+                    </button>
+                  </div>
                 </div>
-              </div>
-            }
-            {
-              this.props.selectedTopicID
-              &&
-              <div className="row">
-                <div className="col-12 pb-4">
-                  {/*For Padding*/}
+              }
+              {
+                this.props.selectedTopicID
+                &&
+                <div className="row">
+                  <div className="col-12 pb-4">
+                    {/*For Padding*/}
+                  </div>
                 </div>
-              </div>
-            }
-          </div>
-        }
-      </div>
+              }
+            </div>
+          }
+        </div>
     )
   }
 }
 
 const stateToPropertyMapper = (state) => {
   return {
-    selectedTopicID: state.topicReducer.selectedTopicID
+    selectedTopicID: state.topicReducer.selectedTopicID,
+    widgetList: state.widgetReducer.widgets
   }
-}
+};
 
-export default connect(stateToPropertyMapper)(ModuleWidget)
+const dispatchToPropertyMapper = (dispatch) => {
+  return {
+    createNewWidget: (topicID) => {
+      const newAddedWidget = {
+        "name": "New Widget",
+        "type": "New Widget",
+        "order": 0,
+        "text": "New Widget Content",
+        "source": "",
+        "size": 0,
+        "width": 0,
+        "height": 0,
+        "cssClass": "",
+        "style": "",
+        "value": "",
+        "topicID": `${topicID}`
+      };
+      WidgetService.createWidget(topicID, newAddedWidget)
+        .then(
+            newAddedWidget => dispatch(createNewWidget(newAddedWidget))
+        );
+    }
+  }
+};
+
+export default connect(stateToPropertyMapper, dispatchToPropertyMapper)(
+    ModuleWidget)
